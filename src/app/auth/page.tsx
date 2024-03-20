8e93f8b139b12a38c15b51075a7cc7084ce4bf53
VeronikaMishkovec <veronikakireenko@gmail.com>
2024-03-20 12:09:54 +0300

    add component for auth

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+import styles from './style.module.scss'
+
+export default function Auth() {
+    return (
+      <main className={styles.main}>
+        <div>Auth</div>
+      </main>
+    );
+  }
+  
